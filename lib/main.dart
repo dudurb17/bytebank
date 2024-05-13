a72f04c844b168b7f83d3191f926804a3b4d66c9
dudurb17 <eduardo.robettibedin@gmail.com>
2024-05-13 11:53:39 -0300

    utilizando a refatoração

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          body: const Card(child: ListTile()),
+          body: ListaTransferencia(),
           appBar: AppBar(
-            title: const Text('Tranferencias'),
-            backgroundColor: Colors.blue.shade200,
-          ),
+              title: const Text('Tranferencias'),
+              backgroundColor: Colors.blue.shade200),
           floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
             backgroundColor: Colors.blue.shade200,
@@ -17,3 +17,30 @@ void main() => runApp(
         ),
       ),
     );
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      ItemTransferencia(titulo: "123213", numeroConta: "20"),
+    ]);
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final String titulo;
+  final String numeroConta;
+
+  ItemTransferencia({required this.titulo, required this.numeroConta});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(titulo),
+      subtitle: Text(numeroConta),
+    ));
+  }
+}
