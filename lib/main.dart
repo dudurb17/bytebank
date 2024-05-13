f334388838ddec1845f3b94a63a4a4b3cf98e6e2
dudurb17 <eduardo.robettibedin@gmail.com>
2024-05-13 12:24:36 -0300

    criando stateless

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,48 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main() => runApp(
-      MaterialApp(
+void main() => runApp(ByteBankApp());
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
+  }
+}
+
+class ByteBankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
         home: Scaffold(
-          body: ListaTransferencia(),
-          appBar: AppBar(
-              title: const Text('Tranferencias'),
-              backgroundColor: Colors.blue.shade200),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            backgroundColor: Colors.blue.shade200,
-            onPressed: () => {},
-          ),
-        ),
-      ),
-    );
+      body: FormularioTransferencia(),
+    ));
+  }
+}
 
 class ListaTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      ItemTransferencia(TransFerencia(valor: 50.30, numeroConta: 65)),
-      ItemTransferencia(TransFerencia(valor: 79090.30, numeroConta: 85)),
-      ItemTransferencia(TransFerencia(valor: 8762, numeroConta: 40)),
-    ]);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tranferencias'),
+        backgroundColor: Colors.blue.shade200,
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(TransFerencia(valor: 50.30, numeroConta: 65)),
+          ItemTransferencia(TransFerencia(valor: 79090.30, numeroConta: 85)),
+          ItemTransferencia(TransFerencia(valor: 8762, numeroConta: 40)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue.shade200,
+        onPressed: () => {},
+      ),
+    );
   }
 }
 
