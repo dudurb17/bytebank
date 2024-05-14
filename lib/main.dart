e75b51350ad97bd9f63e24be5f2042ce15857442
dudurb17 <eduardo.robettibedin@gmail.com>
2024-05-14 10:30:16 -0300

    Refatorando os cards

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class FormularioTransferencia extends StatelessWidget {
   final TextEditingController _controladorCampoNumeroConta =
       TextEditingController();
   final TextEditingController _controladorCampoValor = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -16,46 +17,63 @@ class FormularioTransferencia extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: _controladorCampoNumeroConta,
-                style: TextStyle(fontSize: 24.0),
-                decoration: const InputDecoration(
-                    labelText: "Número da conta", hintText: "0000"),
-                keyboardType: TextInputType.number,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: _controladorCampoValor,
-                style: TextStyle(fontSize: 24.0),
-                decoration: const InputDecoration(
-                    icon: Icon(Icons.monetization_on),
-                    labelText: "Valor",
-                    hintText: "0.00"),
-                keyboardType: TextInputType.number,
-              ),
+            Editor(
+                controlador: _controladorCampoNumeroConta,
+                rotulo: "Numero da conta",
+                dica: "0000"),
+            Editor(
+              controlador: _controladorCampoValor,
+              rotulo: "Valor",
+              dica: "0.00",
+              icon: Icons.monetization_on,
             ),
             ElevatedButton(
-              onPressed: () {
-                debugPrint("clicou no confirmar");
-
-                final int? numeroConta =
-                    int.tryParse(_controladorCampoNumeroConta.text);
-                final double? valor =
-                    double.tryParse(_controladorCampoValor.text);
-                if (numeroConta != null && valor != null) {
-                  final transFerenciaCriada =
-                      TransFerencia(numeroConta: numeroConta, valor: valor);
-                  debugPrint("$transFerenciaCriada");
-                }
-              },
+              onPressed: () => _criaTransferencia(),
               child: Text("Confirmar"),
             )
           ],
         ));
+  }
+
+  void _criaTransferencia() {
+    final int? numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double? valor = double.tryParse(_controladorCampoValor.text);
+    if (numeroConta != null && valor != null) {
+      final transFerenciaCriada =
+          TransFerencia(numeroConta: numeroConta, valor: valor);
+      debugPrint("$transFerenciaCriada");
+      debugPrint("$transFerenciaCriada");
+    }
+  }
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData? icon;
+
+  Editor(
+      {required this.controlador,
+      required this.rotulo,
+      required this.dica,
+      this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controlador,
+        style: TextStyle(fontSize: 24.0),
+        decoration: InputDecoration(
+          icon: icon != null ? Icon(icon) : null,
+          labelText: rotulo,
+          hintText: dica,
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    );
   }
 }
 
